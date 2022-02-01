b918a503cdae3912152a9ab071fdb254d57a871f
BunkerLabz <98615391+BunkerLabz@users.noreply.github.com>
2022-02-01 12:47:17 +0530

    Time: 11 ms (61.57%), Space: 44.8 MB (7.01%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -1,10 +1,9 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        int r = 0, sum = 0;
+        int sum = 0;
         int temp = x;
         while(x>0) {
-            r = x%10;
-            sum = (sum * 10) + r;
+            sum = (sum * 10) + x % 10;
             x = x/10;
         }
         if(sum == temp)
