7d9a5db236bb12b5f8649ba95e1e09ee654135e3
BunkerLabz <98615391+BunkerLabz@users.noreply.github.com>
2022-02-04 12:05:16 +0530

    Time: 2 ms (66.13%), Space: 55.2 MB (5.97%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,18 +1,6 @@
 class Solution {
-    public void moveZeroes(int[] nums) {
-        // int n = nums.length;
-        // for(int i = 0; i < n; i++) {
-        //     for(int j = i + 1; j < n; j++) {
-        //         if(nums[i] == 0 && nums[j] != 0) {
-        //             nums[i] = nums[j];
-        //             nums[j] = 0;
-        //         }
-        //         else
-        //             continue;
-        //     }
-        // }
+    public void moveZeroes(int[] nums) {        
         int zero = 0;
-        // int n  = nums.length;
         for(int i = 0; i < nums.length; i++) {
             if(nums[i] == 0) {
                 zero++;
