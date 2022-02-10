13cfdb9b7a1b48d8d201695dee3f89fa5167b1f0
BunkerLabz <98615391+BunkerLabz@users.noreply.github.com>
2022-02-10 14:54:22 +0530

    Time: 2 ms (43.52%), Space: 43.2 MB (9.32%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.java
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.java
@@ -3,10 +3,10 @@ class Solution {
         int x = 0;
         for(int i = 0; i < operations.length; i++) {
             if(operations[i].equals("X--") || operations[i].equals("--X")) {
-                x = x - 1;
+                x--;
             }
             else
-                x = x + 1;
+                x++;
         }
         return x;
     }
