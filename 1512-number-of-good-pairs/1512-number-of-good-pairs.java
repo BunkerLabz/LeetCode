2dd90aa999180b931f5f05b91c226d5f581a4dd1
BunkerLabz <98615391+BunkerLabz@users.noreply.github.com>
2022-02-13 15:20:37 +0530

    Time: 1 ms (81.96%), Space: 39.8 MB (25.40%) - LeetHub

---
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int n = nums.length;
+        int count = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                if(nums[i] == nums[j])
+                    count++;
+                else
+                    continue;
+            }
+        }
+        return count;
+    }
+}
