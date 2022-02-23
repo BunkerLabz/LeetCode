ee3c98b8b1eeae74df272f656aab1ac40ff63b5e
BunkerLabz <98615391+BunkerLabz@users.noreply.github.com>
2022-02-23 23:46:31 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (43.01%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.java
+++ 58-length-of-last-word/58-length-of-last-word.java
@@ -1,16 +1,7 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        int count = 0;
-        for(int i = s.length() - 1; i >= 0; i--) {
-            if(count == 0 && s.charAt(i) == ' ')
-                continue;
-            
-            else if(count > 0 && s.charAt(i) == ' ')
-                return count;
-            
-            else
-                count++;
-        }
-        return count;
+        s = s.trim();
+        int n = s.lastIndexOf(' ') + 1;
+        return s.length() - n;
     }
 }
