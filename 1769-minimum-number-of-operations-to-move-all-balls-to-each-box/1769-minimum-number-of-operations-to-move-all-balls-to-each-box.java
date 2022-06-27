03de0cbb5e88dca61ae5eb9451d246f76b1e131a
BunkerLabz <98615391+BunkerLabz@users.noreply.github.com>
2022-06-27 18:55:14 +0530

    Time: 2 ms (99.80%), Space: 43 MB (85.21%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,19 +1,41 @@
 class Solution {
     public int[] minOperations(String boxes) {
-        int count = 0;
         int n = boxes.length();
-        int nums[] = new int[n];
+        int[] left = new int[n];
+        int[] right = new int[n];
+
+        int count = boxes.charAt(0) - '0';
+        for(int i = 1 ; i < n ; i++){
+            left[i] = left[i - 1] + count;
+            count += boxes.charAt(i) - '0';
+        }
+
+        count = boxes.charAt(n - 1) - '0';
+        for(int i = n - 2 ; i >=0 ; i--){
+            right[i] = right[i + 1] + count;
+            count += boxes.charAt(i) - '0';
+        }
+        
         for(int i = 0; i < n; i++) {
-			for(int j = 0; j < n; j++) {
-				if(i == j || boxes.charAt(j) == '0') {
-					continue;
-				}
-				else
-					count = count + Math.abs(i - j);
-			}
-			nums[i] = count;
-			count = 0;
-		}
-        return nums;
+            left[i] += right[i];
+        }
+        
+        return left;
+        
+        
+        
+		// int nums[] = new int[n];
+		// for(int i = 0; i < n; i++) {
+		// 	for(int j = 0; j < n; j++) {
+		// 		if(i == j || boxes.charAt(j) == '0') {
+		// 			continue;
+		// 		}
+		// 		else
+		// 			count = count + Math.abs(i - j);
+		// 	}
+		// 	nums[i] = count;
+		// 	count = 0;
+		// }
+		// return nums;
     }
 }
