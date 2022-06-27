cf067a8a7bed867d1093cf9058a147b6ed859bec
BunkerLabz <98615391+BunkerLabz@users.noreply.github.com>
2022-06-27 15:54:00 +0530

    Time: 515 ms (7.29%), Space: 47.6 MB (38.99%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -4,10 +4,7 @@ class Solution {
         int nums[] = new int[boxes.length()];
         for(int i = 0; i < boxes.length(); i++) {
 			for(int j = 0; j < boxes.length(); j++) {
-				if(i == j) {
-					continue;
-				}
-				else if(boxes.charAt(j) == '0') {
+				if(i == j || boxes.charAt(j) == '0') {
 					continue;
 				}
 				else
