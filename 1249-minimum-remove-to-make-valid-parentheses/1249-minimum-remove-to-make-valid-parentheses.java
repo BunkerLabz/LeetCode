00e09564219b1f336faf4b16f7b6d1301a7ba920
BunkerLabz <98615391+BunkerLabz@users.noreply.github.com>
2022-06-28 21:26:49 +0530

    Time: 51 ms (29.24%), Space: 54.6 MB (25.48%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -1,24 +1,17 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
         char ch[] = s.toCharArray();
-        Stack<Character> st = new Stack<>();
-        Stack<Integer> stCount = new Stack<>();
+        Stack<Integer> st = new Stack<>();
         for(int i = 0; i < s.length(); i++) {
             if(ch[i] == '(' || ch[i] == ')') {
-                if(st.isEmpty()) {
-                    st.push(ch[i]);
-                    stCount.push(i);
-                }
-                else if(st.isEmpty() && ch[i] == ')') {
+                if(st.isEmpty() && ch[i] == ')') {
                     ch[i] = ' ';
                 }
-                else if(st.peek() == '(' && ch[i] == ')') {
-                    st.pop();
-                    stCount.pop();
+                else if(ch[i] == '(') {
+                    st.push(i);
                 }
                 else {
-                    st.push(ch[i]);
-                    stCount.push(i);
+                    st.pop();
                 }
             }
             else
@@ -29,10 +22,10 @@ class Solution {
             return String.valueOf(ch).replaceAll(" ", "");
         }
         else {
-            while(!stCount.isEmpty()) {
-                i = stCount.peek();
+            while(!st.isEmpty()) {
+                i = st.peek();
                 ch[i] = ' ';  
-                stCount.pop();
+                st.pop();
             }
         }
         return String.valueOf(ch).replaceAll(" ", "");
