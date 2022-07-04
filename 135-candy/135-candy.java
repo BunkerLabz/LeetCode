a4bcd1280754572a6cfe907d74981ef1286a655b
BunkerLabz <98615391+BunkerLabz@users.noreply.github.com>
2022-07-04 13:05:51 +0530

    Time: 5 ms (45.91%), Space: 51.9 MB (38.56%) - LeetHub

--- 135-candy/135-candy.java
+++ 135-candy/135-candy.java
@@ -1,9 +1,9 @@
 class Solution {
     public int candy(int[] ratings) {
         int n = ratings.length;
-        int l2r[] = new int[n];
+        int candiesR[] = new int[n];
         int candiesL[] = new int[n];
-        Arrays.fill(l2r, 1);
+        Arrays.fill(candiesR, 1);
         Arrays.fill(candiesL, 1);
         for(int i = 1; i < n; i++) {
             if(ratings[i] > ratings[i - 1]) {
@@ -11,13 +11,13 @@ class Solution {
             }
         }
         int sum = 0;
-        candiesL[n - 1] = Math.max(l2r[n - 1], candiesL[n - 1]);
+        candiesL[n - 1] = Math.max(candiesR[n - 1], candiesL[n - 1]);
         sum = candiesL[n - 1];
         for(int i = n - 2; i >= 0; i--) {
             if(ratings[i] > ratings[i + 1]) {
-                l2r[i] += l2r[i + 1]; 
+                candiesR[i] += candiesR[i + 1]; 
             }
-            candiesL[i] = Math.max(candiesL[i], l2r[i]);
+            candiesL[i] = Math.max(candiesL[i], candiesR[i]);
             sum += candiesL[i];
         }
         return sum;
