27ee1c0a35bd4fa2389318c9cce9d99f2c2f1dd6
BunkerLabz <98615391+BunkerLabz@users.noreply.github.com>
2022-07-04 13:13:45 +0530

    Time: 6 ms (29.60%), Space: 51.4 MB (71.36%) - LeetHub

--- 135-candy/135-candy.java
+++ 135-candy/135-candy.java
@@ -1,25 +1,43 @@
 class Solution {
     public int candy(int[] ratings) {
+        // int n = ratings.length;
+        // int candiesR[] = new int[n];
+        // int candiesL[] = new int[n];
+        // Arrays.fill(candiesR, 1);
+        // Arrays.fill(candiesL, 1);
+        // for(int i = 1; i < n; i++) {
+        //     if(ratings[i] > ratings[i - 1]) {
+        //         candiesL[i] += candiesL[i - 1]; 
+        //     }
+        // }
+        // int sum = 0;
+        // candiesL[n - 1] = Math.max(candiesR[n - 1], candiesL[n - 1]);
+        // sum = candiesL[n - 1];
+        // for(int i = n - 2; i >= 0; i--) {
+        //     if(ratings[i] > ratings[i + 1]) {
+        //         candiesR[i] += candiesR[i + 1]; 
+        //     }
+        //     candiesL[i] = Math.max(candiesL[i], candiesR[i]);
+        //     sum += candiesL[i];
+        // }
+        
         int n = ratings.length;
-        int candiesR[] = new int[n];
-        int candiesL[] = new int[n];
-        Arrays.fill(candiesR, 1);
-        Arrays.fill(candiesL, 1);
+        int candies[] = new int[n];
+        Arrays.fill(candies, 1);
         for(int i = 1; i < n; i++) {
             if(ratings[i] > ratings[i - 1]) {
-                candiesL[i] += candiesL[i - 1]; 
+                candies[i] += candies[i - 1]; 
             }
         }
-        int sum = 0;
-        candiesL[n - 1] = Math.max(candiesR[n - 1], candiesL[n - 1]);
-        sum = candiesL[n - 1];
         for(int i = n - 2; i >= 0; i--) {
             if(ratings[i] > ratings[i + 1]) {
-                candiesR[i] += candiesR[i + 1]; 
+                candies[i] = Math.max(candies[i], (candies[i + 1] + 1));
             }
-            candiesL[i] = Math.max(candiesL[i], candiesR[i]);
-            sum += candiesL[i];
         }
+        int sum = 0;        
+        for (int candy : candies)  
+    	    sum += candy;     
+        
         return sum;
     }
 }
