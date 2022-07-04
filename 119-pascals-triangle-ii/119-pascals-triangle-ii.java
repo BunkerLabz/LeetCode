40d978b7c7dc4277bbbf362f6bc9cc5ff9bb9fba
BunkerLabz <98615391+BunkerLabz@users.noreply.github.com>
2022-07-04 17:41:37 +0530

    Time: 2 ms (56.38%), Space: 41.7 MB (61.55%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.java
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.java
@@ -1,21 +1,13 @@
-class Solution {
-    public List<Integer> getRow(int rowIndex) {
-        ArrayList<Integer> l;
-        ArrayList<Integer> pl = new ArrayList<>();
-        pl.add(1);
-        for(int i = 1; i < rowIndex + 1; i++) {
-            l = new ArrayList<>();
-            for(int j = 0; j <= i; j++) {
-                if(j == 0 || j == i) {
-                    l.add(1);
-                }
-                else {
-                    l.add(pl.get(j - 1) + pl.get(j));
-                }
-            }
-            pl = l;
-        }
-
-        return pl;
+public class Solution {
+    public List<Integer> getRow(int k) {
+        Integer[] arr = new Integer[k + 1];
+        Arrays.fill(arr, 0);
+        arr[0] = 1;
+        
+        for (int i = 1; i <= k; i++) 
+            for (int j = i; j > 0; j--) 
+                arr[j] = arr[j] + arr[j - 1];
+        
+        return Arrays.asList(arr);
     }
 }
