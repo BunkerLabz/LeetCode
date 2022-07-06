fb2e196b4a54d61a056dd5727cd02c815e5eb0e6
BunkerLabz <98615391+BunkerLabz@users.noreply.github.com>
2022-07-06 15:46:25 +0530

    Time: 1 ms (100.00%), Space: 42.5 MB (89.31%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,22 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        
-        Set<Integer> hashSet = new HashSet<>();
-        
+        int res = 0;
         for(int i = 0; i < nums.length; i++) {
-            
-            if(hashSet.contains(nums[i])) {
-                hashSet.remove(nums[i]);
-            }
-            
-            else {
-                hashSet.add(nums[i]);
-            }
-            
+            res ^= nums[i];
         }
-        nums = new int[1];
-        nums = hashSet.stream().mapToInt(Integer::intValue).toArray();
-        return nums[0];
+        return res;
     
     }
 }
