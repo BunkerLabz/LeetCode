d2039b6e332199e33fe20e5de358eef152ac653d
BunkerLabz <98615391+BunkerLabz@users.noreply.github.com>
2022-07-18 23:18:48 +0530

    Time: 29 ms (15.07%), Space: 43.1 MB (76.80%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
@@ -1,23 +1,50 @@
+// class Solution {
+//     public String reverseWords(String s) {
+//         s = s.trim();
+//         int br = s.length();
+//         String reset = new String();
+//         for(int i = s.length() - 1; i >= 0; i--) {
+//             if(s.charAt(i) == ' ') {
+//                 if(s.charAt(i + 1) == ' ') {
+//                     br = i;
+//                     continue; 
+//                 }
+//                 else {
+//                     reset += s.substring(i + 1, br) + " ";
+//                     br = i;
+//                 }
+//             }
+//             if(i == 0) {
+//                 reset += s.substring(0, br);   
+//             }
+//         }
+//         return reset;
+//     }
+// }
+
 class Solution {
     public String reverseWords(String s) {
         s = s.trim();
+        int n = s.length();
         int br = s.length();
-        String reset = new String();
-        for(int i = s.length() - 1; i >= 0; i--) {
+        System.out.println(n);
+        s += " -1 ";
+        for(int i = n - 1; i >= 0; i--) {
             if(s.charAt(i) == ' ') {
                 if(s.charAt(i + 1) == ' ') {
                     br = i;
-                    continue; 
+                    continue;
                 }
                 else {
-                    reset += s.substring(i + 1, br) + " ";
+                    s += s.substring(i + 1, br) + " ";
+                    System.out.println(br);
                     br = i;
                 }
             }
             if(i == 0) {
-                reset += s.substring(0, br);   
+                s += s.substring(0, br);
             }
         }
-        return reset;
+        return s.substring(n + 4);
     }
 }
