75f10536e4111f9caf8a23c0c5ae8ee65cabd8fe
BunkerLabz <98615391+BunkerLabz@users.noreply.github.com>
2022-07-21 21:36:50 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (68.69%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.java
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.java
@@ -8,34 +8,48 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+// -- Brute Force --
+// class Solution {
+//     public ListNode middleNode(ListNode head) {
+//         if(head.next == null)
+//             return head;
+//         ListNode curNode = head;
+//         int count = 0;
+//         while(curNode != null) {
+//             curNode = curNode.next;
+//             count++;
+//         }
+//         int mid = 0;
+//         if(count % 2 == 0) {
+//             mid = count / 2 + 1;
+//             curNode = head;
+//             while(mid > 1) {
+//                 curNode = curNode.next;
+//                 mid--;
+//             }
+//             return curNode;
+//         }
+//         else
+//             mid = count / 2;
+//         curNode = head;
+//         while(mid > 0) {
+//             curNode = curNode.next;
+//             mid--;
+//         }
+//         System.out.println(curNode.val);
+//         return curNode;
+//     }
+// }
+// -- Brute Force --
+
 class Solution {
     public ListNode middleNode(ListNode head) {
-        if(head.next == null)
-            return head;
-        ListNode curNode = head;
-        int count = 0;
-        while(curNode != null) {
-            curNode = curNode.next;
-            count++;
+        ListNode slow = head, fast = head;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        int mid = 0;
-        if(count % 2 == 0) {
-            mid = count / 2 + 1;
-            curNode = head;
-            while(mid > 1) {
-                curNode = curNode.next;
-                mid--;
-            }
-            return curNode;
-        }
-        else
-            mid = count / 2;
-        curNode = head;
-        while(mid > 0) {
-            curNode = curNode.next;
-            mid--;
-        }
-        System.out.println(curNode.val);
-        return curNode;
+        return slow;
     }
 }
