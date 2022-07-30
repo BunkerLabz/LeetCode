d61377020be7246391f2960991a6308d2e80fd81
BunkerLabz <98615391+BunkerLabz@users.noreply.github.com>
2022-07-30 22:18:27 +0530

    Time: 2 ms (78.59%), Space: 42.8 MB (70.29%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.java
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.java
@@ -1,20 +1,31 @@
 class Solution {
     public List<String> findAndReplacePattern(String[] words, String pattern) {
-        int[] p = F(pattern);
-        List<String> res = new ArrayList<String>();
-        for (String w : words)
-            if (Arrays.equals(F(w), p)) res.add(w);
-        return res;
-    }
-
-    public int[] F(String w) {
-        HashMap<Character, Integer> m = new HashMap<>();
-        int n = w.length();
-        int[] res = new int[n];
-        for (int i = 0; i < n; i++) {
-            m.putIfAbsent(w.charAt(i), m.size());
-            res[i] = m.get(w.charAt(i));
+        List<String> list = new ArrayList<>();
+        
+        for(String s : words) {
+            if(match(s, pattern)) {
+                list.add(s);
+            }
         }
-        return res;
+        
+        return list;
+    }
+    
+    private boolean match(String s, String pattern) {
+        char patternToWord[] = new char[26];
+        char wordToPattern[] = new char[26];
+        
+        for(int i = 0; i < s.length(); i++) {
+            if(patternToWord[pattern.charAt(i) - 'a'] == 0) {
+                patternToWord[pattern.charAt(i) - 'a'] = s.charAt(i);
+            }
+            if(wordToPattern[s.charAt(i) - 'a'] == 0) {
+                wordToPattern[s.charAt(i) - 'a'] = pattern.charAt(i);
+            }
+            if(patternToWord[pattern.charAt(i) - 'a'] != s.charAt(i) || wordToPattern[s.charAt(i) - 'a'] != pattern.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
