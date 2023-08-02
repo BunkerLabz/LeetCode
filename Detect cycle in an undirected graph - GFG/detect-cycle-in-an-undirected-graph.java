1ef5436a75d43d1324fd0568076412c406692610
BunkerLabz <98615391+BunkerLabz@users.noreply.github.com>
2023-08-03 00:39:32 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
@@ -36,40 +36,60 @@ class Solution {
     // Function to detect cycle in an undirected graph.
     public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
         
-        boolean[] vis = new boolean[V];
+        int[] vis = new int[V];
         
-        for(int i = 0; i < V; i++) vis[i] = false;
+        for(int i = 0; i < V; i++) vis[i] = 0;
         
         for(int i = 0; i  < V; i++) {
-            if(vis[i] == false) {
-                if(check(i, V, vis, adj)) return true;
-            }
-        }
-        
-        return false;
-        
-    }
-    public boolean check(int i, int V, boolean[] vis, ArrayList<ArrayList<Integer>> adj) {
-        Queue<Pair> q = new LinkedList<>();
-        vis[i] = true;
-        q.add(new Pair(i, -1));
-        while(!q.isEmpty()) {
-            int node = q.peek().first;
-            int parent = q.peek().next;
-            q.remove();
-            
-            for(int adjNode : adj.get(node)) {
-                if(vis[adjNode] == false) {
-                    vis[adjNode]= true;
-                    q.add(new Pair(adjNode, node));
-                }
-                else if(parent != adjNode){
+            if(vis[i] == 0) {
+                // if(check(i, V, vis, adj)) return true;
+                if(dfs(i, -1, vis, adj) == true) {
                     return true;
-                } 
+                }
             }
         }
+    
         return false;
+        
     }
+    
+    private boolean dfs(int node, int parent, int vis[], ArrayList<ArrayList<Integer>> 
+    adj) {
+        vis[node] = 1; 
+        // go to all adjacent nodes
+        for(int adjacentNode: adj.get(node)) {
+            if(vis[adjacentNode]==0) {
+                if(dfs(adjacentNode, node, vis, adj) == true) 
+                    return true; 
+            }
+            // if adjacent node is visited and is not its own parent node
+            else if(adjacentNode != parent) return true; 
+        }
+        return false; 
+    }
+    
+    
+    // public boolean check(int i, int V, boolean[] vis, ArrayList<ArrayList<Integer>> adj) {
+    //     Queue<Pair> q = new LinkedList<>();
+    //     vis[i] = true;
+    //     q.add(new Pair(i, -1));
+    //     while(!q.isEmpty()) {
+    //         int node = q.peek().first;
+    //         int parent = q.peek().next;
+    //         q.remove();
+            
+    //         for(int adjNode : adj.get(node)) {
+    //             if(vis[adjNode] == false) {
+    //                 vis[adjNode]= true;
+    //                 q.add(new Pair(adjNode, node));
+    //             }
+    //             else if(parent != adjNode){
+    //                 return true;
+    //             } 
+    //         }
+    //     }
+    //     return false;
+    // }
 }
 
 class Pair{
